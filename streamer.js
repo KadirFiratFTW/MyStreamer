a76688c06f5f68144132e0877874d16d897af451
BenKF <67444810+KadirFiratFTW@users.noreply.github.com>
2021-04-06 18:00:05 +0300

    Update streamer.js
    added delete streamer option.

--- streamer.js
+++ streamer.js
@@ -4,13 +4,14 @@ const StreamerName = document.querySelector(".streamer-name");
 
 chrome.storage.local.get('streamers', function (data) {
   data.streamers.forEach(S => {
-    const Template = `<div class="streamer" data-streamer="` + S.name + `" data-href="https://twitch.tv/` + S.name + `">
+    const Template = `<div id="streamer" class="streamer" data-streamer="` + S.name + `">
       <img
-        src="`+ S.logo + `" width="48">
-      <div class="streamer-status">
-        <b>`+ S.name + `</b>
-        <b class="streamer-live">`+ (S.status ? 'LIVE' : 'OFFLINE') + `</b>
+        src="`+ S.logo + `" width="48" data-streamer="` + S.name + `">
+      <div class="streamer-status" data-streamer="` + S.name + `">
+        <b data-streamer="` + S.name + `">`+ S.name + `</b>
+        <b class="streamer-live" data-streamer="` + S.name + `">`+ (S.status ? 'LIVE' : 'OFFLINE') + `</b>
       </div>
+      <div id="close" class="close" data-name="`+ S.name + `">X</div>
     </div>`
 
     document.querySelector(".streamer-list").insertAdjacentHTML("beforeend", Template);
@@ -23,6 +24,25 @@ chrome.storage.local.get('alerts', function (data) {
   NotifiesSetter.checked = data.alerts;
 });
 
+document.addEventListener('click', function (e) {
+  if (e.target && e.target.dataset.streamer) {
+    window.open("https://twitch.tv/" + e.target.dataset.streamer);
+    return;
+  }
+  if (e.target && e.target.id == 'close') {
+    removeStreamer(e.target.dataset.name);
+    return;
+  }
+});
+
+function removeStreamer(s) {
+  chrome.storage.local.get('streamers', function (data) {
+    let Streamers = [...data.streamers.filter(S => S.name !== s)];
+    document.querySelector(".streamer[data-streamer='" + s + "']").remove();
+    chrome.storage.local.set({ 'streamers': Streamers }, function () {
+    });
+  });
+}
 
 async function addStreamer() {
 
@@ -37,14 +57,16 @@ async function addStreamer() {
 
     chrome.storage.local.get('streamers', function (data) {
       let Streamers = [...data.streamers];
+      if (Streamers.find(S => S.name == StreamerName.value.toLowerCase())) return;
       Streamers.push({ name: StreamerName.value.toLowerCase(), status: false, logo: ParseText.users[0].logo });
-      const Template = `<div class="streamer" data-streamer="` + StreamerName.value.toLowerCase() + `" data-href="https://twitch.tv/` + StreamerName.value.toLowerCase() + `">
+      const Template = `<div id="streamer" class="streamer" data-streamer="` + StreamerName.value.toLowerCase() + `">
       <img
-        src="`+ ParseText.users[0].logo + `" width="48">
-      <div class="streamer-status">
-        <b>`+ StreamerName.value.toLowerCase() + `</b>
-        <b class="streamer-live">CHECKING</b>
+        src="`+ ParseText.users[0].logo + `" width="48" data-streamer="` + StreamerName.value.toLowerCase() + `">
+      <div data-streamer="` + StreamerName.value.toLowerCase() + `" class="streamer-status">
+        <b data-streamer="` + StreamerName.value.toLowerCase() + `">`+ StreamerName.value.toLowerCase() + `</b>
+        <b data-streamer="` + StreamerName.value.toLowerCase() + `" class="streamer-live">Kontrol ediliyor..</b>
       </div>
+      <div class="close" id="close" data-name="`+ StreamerName.value.toLowerCase() + `">X</div>
     </div>`
 
       document.querySelector(".streamer-list").insertAdjacentHTML("beforeend", Template);
@@ -64,7 +86,6 @@ setInterval(function () {
     data.streamers.forEach(S => {
       document.querySelector("[data-streamer='" + S.name + "'] img").src = S.logo;
       document.querySelector("[data-streamer='" + S.name + "'] .streamer-live").innerHTML = S.status ? "LIVE" : "OFFLINE";
-
     })
   });
 }, 5000);
